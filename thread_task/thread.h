cd077ff373587df8d9e8a843f8e2bdafc899d0cc
TheatreLight <scopolamine@mail.ru>
2023-03-08 17:59:21 +0600

    create class threadpool

--- thread_task/thread.h
+++ thread_task/thread.h
@@ -1,8 +1,25 @@
 #ifndef THREAD_TASK_THREAD_H_
 #define THREAD_TASK_THREAD_H_
 
-class Thread {
+#include <thread>
+#include <functional>
+#include <vector>
 
+class ThreadPool {
+ public:
+  ThreadPool();
+  ThreadPool(const ThreadPool& other);
+  ThreadPool(ThreadPool&& other) = delete;
+  ~ThreadPool() = default;
+  ThreadPool operator=(const ThreadPool& other);
+  ThreadPool operator=(ThreadPool&& other) = delete;
+
+  uint64_t AddTask(std::function<void(int)> foo);
+  void Wait(uint64_t task_id);
+  void WaitAll();
+
+ private:
+  std::vector<std::thread> pool_;
 };
 
 #endif  // THREAD_TASK_THREAD_H_
