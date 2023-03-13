cb871ae2556b65636e8f51b2556d9ef83f4b633d
TheatreLight <scopolamine@mail.ru>
2023-03-13 15:46:48 +0600

    ready

--- thread_task/thread.cc
+++ thread_task/thread.cc
@@ -1,1 +1,47 @@
 #include "thread.h"
+
+ThreadPool::ThreadPool(uint num) {
+  for (int i = 0; i < num; ++i) {
+    pool_.emplace_back(&Go, this);
+  }
+}
+
+ThreadPool::~ThreadPool() {
+  quit_ = true;
+}
+
+void ThreadPool::Go() {
+  while (!quit_) {
+    std::unique_lock<std::mutex> ul(mtx1_);
+    cv_.wait(ul, wake_up_);
+    if (!f_queue_.empty()) {
+      auto task = f_queue_.front();
+      f_queue_.pop();
+      ul.unlock();
+      task.first();
+      std::lock_guard<std::mutex> guard(mtx2_);
+      done_tasks_ids_.push_back(task.second);
+    }
+    if (f_queue_.empty()) wake_up_ = false;
+  }
+}
+
+uint64_t ThreadPool::AddTask(const std::function<void()>& foo) {
+  id_++;
+  f_queue_.emplace(foo, id_);
+  wake_up_ = true;
+  cv_.notify_one();
+  return id_;
+}
+
+void ThreadPool::Wait(uint64_t task_id) {
+  while (done_tasks_ids_.back() != task_id) {
+    std::this_thread::yield();
+  }
+}
+
+void ThreadPool::WaitAll() {
+  while (!f_queue_.empty()) {
+    std::this_thread::yield();
+  }
+}
--- thread_task/thread.h
+++ thread_task/thread.h
@@ -3,23 +3,35 @@
 
 #include <thread>
 #include <functional>
+#include <mutex>
+#include <condition_variable>
 #include <vector>
+#include <queue>
 
 class ThreadPool {
  public:
-  ThreadPool();
+  explicit ThreadPool(uint num = std::thread::hardware_concurrency());
   ThreadPool(const ThreadPool& other);
   ThreadPool(ThreadPool&& other) = delete;
   ~ThreadPool() = default;
   ThreadPool operator=(const ThreadPool& other);
   ThreadPool operator=(ThreadPool&& other) = delete;
-
-  uint64_t AddTask(std::function<void(int)> foo);
+  void Go();
+  uint64_t AddTask(const std::function<void()>& foo);
   void Wait(uint64_t task_id);
   void WaitAll();
 
  private:
   std::vector<std::thread> pool_;
+  std::queue<std::pair<std::function<void()>, uint64_t>> f_queue_;
+  std::vector<int> v;
+  uint64_t id_ = 0;
+  std::mutex mtx1_;
+  std::mutex mtx2_;
+  std::condition_variable cv_;
+  std::vector<uint64_t> done_tasks_ids_;
+  bool wake_up_ = false;
+  bool quit_ = false;
 };
 
 #endif  // THREAD_TASK_THREAD_H_
